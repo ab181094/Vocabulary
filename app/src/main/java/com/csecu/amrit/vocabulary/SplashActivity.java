69cb449d36ef69638bd513cd0d74f1bfa411312f
Amrit Bhattacharjee <ab181094@gmail.com>
2017-08-29 00:45:20 +0600

    second commit. Added delay text and change of activity.

--- app/src/main/java/com/csecu/amrit/vocabulary/SplashActivity.java
+++ app/src/main/java/com/csecu/amrit/vocabulary/SplashActivity.java
@@ -1,15 +1,29 @@
 package com.csecu.amrit.vocabulary;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.csecu.amrit.vocabulary.list.ListActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
+
+    TextView splash_tv_app;
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    private final int SPLASH_CHANGE_LENGTH = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +39,52 @@ public class SplashActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+
+        splash_tv_app = (TextView) findViewById(R.id.splash_tv_app);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                setText("Vocabulary");
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, ListActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_CHANGE_LENGTH);
+    }
+
+    private void setText(final String s) {
+        final int[] i = new int[1];
+        i[0] = 0;
+        final int length = s.length();
+        final Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                char c = s.charAt(i[0]);
+                Log.d("Strange", "" + c);
+                splash_tv_app.append(String.valueOf(c));
+                i[0]++;
+            }
+        };
+
+        final Timer timer = new Timer();
+        TimerTask taskEverySplitSecond = new TimerTask() {
+            @Override
+            public void run() {
+                handler.sendEmptyMessage(0);
+                if (i[0] == length - 1) {
+                    timer.cancel();
+                }
+            }
+        };
+        timer.schedule(taskEverySplitSecond, 1, 200);
     }
 }
